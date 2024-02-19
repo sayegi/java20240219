fa09d51c6c58dd0d1d3ee8e2e263cfb8974b0019
JunSeok Lee <galaxysayegi@gmail.com>
2024-02-19 12:42:03 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,7 @@
+public class Main3 {
+    public  static void main(String[] args) {
+        System.out.println("만나서");
+        System.out.println("반갑습니다");
+        System.out.println("LG 오지환입니다");
+    }
+}
